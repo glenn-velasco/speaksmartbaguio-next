5ede792583e554170dd8260bfc45786f4ad2635e
glenn velasco <glennvelasco525@gmail.com>
2026-01-23 19:49:22 +0800

    feat(phrasebook-endpoint): Implement phrasebook endpoint with phrasebook items search params and limit handling

---
+++ app/api/v1/phrasebook/route.ts
@@ -0,0 +1,76 @@
+import { NextResponse, NextRequest } from "next/server";
+import { adminDb } from "@/lib/firebase-admin";
+import { QueryDocumentSnapshot } from "firebase-admin/firestore";
+
+interface PhraseBookItem {
+  ilokanoWord: string;
+  englishTranslation: string;
+  tagalogTranslation: string;
+  partOfSpeech: string;
+}
+
+export async function GET(request: NextRequest) {
+
+  try {
+
+    const searchParams = request.nextUrl.searchParams;
+
+    const partOfSpeech = searchParams.get("partOfSpeech");
+
+    const english = searchParams.get("englishTranslation");
+
+    const ilokano = searchParams.get("ilokanoWord");
+
+    const tagalog = searchParams.get("tagalogTranslation");
+
+    const limit = parseInt(searchParams.get("limit") || "10");
+
+    let query: any = adminDb.collection("phrasebook");
+
+    if (partOfSpeech) {
+      
+      query = query.where("partOfSpeech", "==", partOfSpeech);
+    }
+ 
+    if (english) {
+      
+      query = query.where("englishTranslation", "==", english);
+    }
+
+    if (ilokano) {
+      
+      query = query.where("ilokanoWord", "==", ilokano);
+    }
+
+    if (tagalog) {
+      
+      query = query.where("tagalogTranslation", "==", tagalog);
+    }
+
+    const searchSnapshot = await query.limit(limit).get();
+
+    if (searchSnapshot.empty) {
+
+      return NextResponse.json({ message: "No matching entries found." }, { status: 404 });
+    }
+
+    const data = searchSnapshot.docs.map((doc: QueryDocumentSnapshot) => {
+
+      const item = doc.data() as PhraseBookItem;
+
+      return {
+        id: doc.id,
+        ...item,
+      };
+    });
+
+    return NextResponse.json({ data }, { status: 200 });
+
+  } catch (error) {
+
+    console.error("Filtering API Error:", error);
+    
+    return NextResponse.json({ error: "Search failed" }, { status: 500 });
+  }
+
+}
